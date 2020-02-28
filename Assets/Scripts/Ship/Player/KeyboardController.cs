b0c14eb1f75cf675f2311385e56ee70db4600a98
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-02-28 12:02:30 -0300

    Fix bug with KeyboardController

--- Assets/Scripts/Ship/Player/KeyboardController.cs
+++ Assets/Scripts/Ship/Player/KeyboardController.cs
@@ -10,14 +10,18 @@ public class KeyboardController : MonoBehaviour
     [SerializeField] private KeyCode left = KeyCode.LeftArrow;
     [SerializeField] private KeyCode right = KeyCode.RightArrow;
 
+    private bool keyPressed;
+    
     private void Update()
     {
+        var hadAKeyPressed = keyPressed;
+        keyPressed = false;
         var direction = Vector2.zero;
         if (Input.GetKey(up)) direction += new Vector2(0, 1);
         if (Input.GetKey(down)) direction += new Vector2(0, -1);
         if (Input.GetKey(left)) direction += new Vector2(-1, 0);
         if (Input.GetKey(right)) direction += new Vector2(1, 0);
-
-        OnMove?.Invoke(direction);
+        if (direction != Vector2.zero) keyPressed = true;
+        if(hadAKeyPressed || keyPressed) OnMove?.Invoke(direction);
     }
 }
