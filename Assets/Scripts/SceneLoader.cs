6bbf09dc766fb04e0ca349fcf7950cbdb1d3c0dc
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-02-28 11:27:35 -0300

    Implement menu interface

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour
+{
+    public enum SceneToLoad
+    {
+        Menu = 0,
+        Level = 1
+    }
+
+    [SerializeField] [Tooltip("Scene to be loaded")]
+    private SceneToLoad sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene((int) sceneToLoad);
+    }
+
+    public static void LoadScene(SceneToLoad scene)
+    {
+        SceneManager.LoadScene((int) scene);
+    }
+}
